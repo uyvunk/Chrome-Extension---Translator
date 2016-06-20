bfa563eedd81772973f03eaa4298bb308492ded9
Khanh Duc Tran <ktran035@cs.uw.edu>
2016-06-21 00:23:11 +0700

    fix position 1

--- translate.js
+++ translate.js
@@ -38,18 +38,9 @@ chrome.runtime.onMessage.addListener(
 			if (result == undefined) {
 				result = '<div class="word_title">Not found</div>';
 			} else {
-				var audio_link = alt_result.match(/http.*\.mp3/);
-				console.log("audio link " + audio_link);
-				var audio = document.createElement("audio");
-				audio.controls = "contols";
-				audio.className = "audio";
-				audio.src = audio_link;
-				var source = document.createElement("source");
-				source.src = audio_link + "";
-				source.type = "audio/mp3";
-				audio.appendChild(source);
-				console.log("here is my audio result: " + audio.outerHTML);
-				result.splice(4, 0, audio);
+				var audio_div = createAudio(alt_result);
+
+				result.splice(4, 0, audio_div);
 				filter(result);
 				//console.log(result);
 			}
@@ -66,7 +57,22 @@ chrome.runtime.onMessage.addListener(
 			pretty();
 		}
 	});
-	
+
+function createAudio(alt_result) {
+	var audio_link = alt_result.match(/http.*\.mp3/);
+	//console.log("audio link " + audio_link);
+	var div = document.createElement("div");
+	div.className = "audio";
+	var audio = document.createElement("audio");
+	audio.controls = "controls";
+	audio.id = "audio";
+	audio.className = "audio";
+	audio.src = audio_link;
+	audio.type = "audio/mp3";
+	div.appendChild(audio);	
+	return div;
+}
+
 // Remove the result div	
 function resetPage() {
 	if ($('#translatorResult').length > 0) {
@@ -81,6 +87,8 @@ function filter(result) {
 	for (var i=0; i<result.length; i++) {
 		var className = result[i].className;
 		console.log("current index " + i + "\n" + className);
+		
+
 		var id = result[i].id;
 		if((className == "dictionary-name" || className == "relatedWord" || className == "idioms" || className == "" ) && id != "tandp") {
 			if(className == "idioms") {
@@ -88,23 +96,28 @@ function filter(result) {
 			}
 			result.splice(i,1);
 			i--;
-		} else if (className == "audio") {
-			//console.log("pronunciation is here: \n" + result[i].innerHTML);
 		} else if (className == "list1") {
 			if (idioms == true) {
 				result.splice(i,1);
 				i--;
 			} else {
 				var li = $.parseHTML(result[i].innerHTML)[0];
-				//console.log("li: \n" + li.innerHTML);
+				console.log("li: \n" + li.innerHTML);
 				var cur = $.parseHTML(li.innerHTML);
-				//console.log("cur: \n" + cur.innerHTML);
+				console.log("cur: \n" + cur.innerHTML);
 				cur.splice(1,cur.length - 1);
 				li.innerHTML = cur[0].outerHTML;
 				//console.log("li outer: \n" + li.outerHTML);
 				result[i].innerHTML = li.outerHTML;
-			}			
-		} else {
+			} 			
+		} else if (className == "pronunciation") {
+			console.log("I'm in pronunciation!!!!")
+			var li = $.parseHTML(result[i].innerHTML)[0];
+			console.log("li: \n" + li);
+			var cur = $.parseHTML(li.innerHTML);
+			console.log("cur: \n" + cur);
+		}
+		else {
 			idioms = false;
 		}
 	}
@@ -112,9 +125,10 @@ function filter(result) {
 
 // Add style to the result DIV
 function pretty() {
-	$('.word_title').css({"color":"#D03071", "font-size":"10pt", "font-weight":"bold"});
-	$('.pronounce').css({"font-style":"italic"});
-	$('.phanloai').css({"color":"#D03071", "font-weight":"bold", "border-top":"1px solid #666", "border-bottom":"1px solid #666", "background":"#eee", "margin":"5px", "padding":"3px"});
+	$('.audio').css({"float":"right", "margin-bottom":"5px", "margin-right":"5px"});
+	$('.word_title').css({"color":"#D03071", "font-size":"10pt", "font-weight":"bold", "clear":"both"});
+	$('.pronounce').css({"font-style":"italic", "display":"inline", "float":"left"});
+	$('.phanloai').css({"color":"#D03071","clear":"both", "font-weight":"bold", "border-top":"1px solid #666", "border-bottom":"1px solid #666", "background":"#eee", "margin":"5px", "padding":"3px"});
 	$('.list1').css({"list-style-type":"circle", "background":"none", "padding":"0px", "margin-left":"30px", "margin-bottom":"15px"});
 	$('.list1 li').css({"list-style-type":"circle","background":"none", "padding":"0px"});
 }
