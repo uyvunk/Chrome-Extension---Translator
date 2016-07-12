4862a062d676d858966785ab6c3a1d690be7bb46
Khanh Duc Tran <ktran035@uw.edu>
2016-07-12 13:40:21 +0700

    successfully send data between content script to background script then to popup script, which will help to develope more functionality like -history search list- -english to english dictionary- and so on

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
+var hist = "";
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(request.message == "query") {
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				var activeTab = tabs[0];
+				// if (storage === null) {
+				// 	storage = [];
+				// }
+				// storage.push(request.data);
+				// localStorage.setItem("hist", storage);
+				// console.log("current list of word " + localStorage.getItem("hist"));
 				lookUp(request.data, activeTab);
-				
 			});
+		} else if (request.message == "history") {
+			hist = request.data;
+		} else if (request.method == "getHist") {
+			sendResponse(hist);
+			getHist(sendResponse);
+			return true;
 		}
 	});
 	
@@ -25,11 +37,11 @@ function lookUp(word, activeTab) {
 		} else {
 			responseText = "";
 		}
-		chrome.tabs.sendMessage(activeTab.id, {"message":"reply", "data":responseText});
+		chrome.tabs.sendMessage(activeTab.id, {"message":"reply", "data":responseText, "word": word});
 	}
 	
 	function receiveError() {
-		chrome.tabs.sendMessage(activeTab.id, {"message":"reply", "data":""});
+		chrome.tabs.sendMessage(activeTab.id, {"message":"reply", "data":"", "word": word});
 	}
 
 	
